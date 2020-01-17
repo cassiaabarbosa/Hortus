6bab3f8824550eeb8512aba465f76b60081e7dfc
Cassia <cassia.a.barbosa@gmail.com>
2020-01-17 18:59:03 -0300

    Trying create the screen

--- MiniChallenge2_Hortus/Helpers/Extensions.swift
+++ MiniChallenge2_Hortus/Helpers/Extensions.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 extension UIColor {
+    
     struct App {
         static var navigation: UIColor { return #colorLiteral(red: 0.6779737473, green: 0.8722688556, blue: 0.7128722072, alpha: 1) }
         static var navigationTitle: UIColor { return #colorLiteral(red: 0.9999018312, green: 1, blue: 0.9998798966, alpha: 1) }
@@ -22,5 +23,44 @@ extension UIColor {
 //        static var actionColor: UIColor { return #colorLiteral(red: 0.8666666667, green: 0.431372549, blue: 0.2588235294, alpha: 1) }
 //        static var black: UIColor { return #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1) }
 //        static var white: UIColor { return #colorLiteral(red: 0.9411764706, green: 0.9411764706, blue: 0.9411764706, alpha: 1) }
+    }
+}
+
+extension UIView {
+    
+    func anchor (top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, right: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingLeft: CGFloat, paddingBottom: CGFloat, paddingRight: CGFloat, width: CGFloat, height: CGFloat, enableInsets: Bool) {
+        var topInset = CGFloat(0)
+        var bottomInset = CGFloat(0)
+        
+        if #available(iOS 12, *), enableInsets {
+            let insets = self.safeAreaInsets
+            topInset = insets.top
+            bottomInset = insets.bottom
+            
+            print("Top: \(topInset)”")
+            print("bottom: \(bottomInset)”")
+        }
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop+topInset).isActive = true
+        }
+        if let left = left {
+            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom-bottomInset).isActive = true
+        }
+        if height != 0 {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+        if width != 0 {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
     }
 }
---
+++ MiniChallenge2_Hortus/Views/TableViewCells/TaskCreatePlantCell.swift
@@ -0,0 +1,107 @@
+//
+//  TableViewCell.swift
+//  MiniChallenge2_Hortus
+//
+//  Created by Cassia Aparecida Barbosa on 17/01/20.
+//  Copyright © 2020 Hortus. All rights reserved.
+//
+
+import UIKit
+
+class TaskCreatePlantCell: UITableViewCell {
+    
+    var taskInformation : TaskInformation? {
+        didSet {
+            taskImage.image = taskInformation?.taskImage
+            taskPeriodLabel.text = taskInformation?.taskPeriod
+            taskNextActionLabel.text = taskInformation?.taskNextAction
+        }
+    }
+    
+    
+    private let taskPeriodLabel : UILabel = {
+    let taskPeriodLabel = UILabel()
+    taskPeriodLabel.textColor = .black
+    taskPeriodLabel.font = UIFont.systemFont(ofSize: 17)
+    taskPeriodLabel.textAlignment = .left
+    taskPeriodLabel.numberOfLines = 0
+    return taskPeriodLabel
+    }()
+    
+    
+    private let taskNextActionLabel: UILabel = {
+        let taskNextActionLabel = UILabel()
+        taskNextActionLabel.textColor = .black
+        taskNextActionLabel.font = UIFont.systemFont(ofSize: 17)
+        taskNextActionLabel.textAlignment = .left
+        taskNextActionLabel.numberOfLines = 0
+        return taskNextActionLabel
+    }()
+    
+    
+    private let taskImage: UIImageView = {
+        let taskImage = UIImageView(image: #imageLiteral(resourceName: "AddFotoPlanta"))
+        taskImage.contentMode = .scaleAspectFit
+        taskImage.clipsToBounds = true
+        taskImage.translatesAutoresizingMaskIntoConstraints = false
+        return taskImage
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+     super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(taskPeriodLabel)
+        addSubview(taskImage)
+        addSubview(taskNextActionLabel)
+        
+        taskImage.centerYAnchor.constraint(equalTo:self.contentView.centerYAnchor).isActive = true
+        taskImage.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: nil, paddingTop: 5, paddingLeft: 5, paddingBottom: 5, paddingRight: 0, width: 90, height: 0, enableInsets: false)
+        
+        taskPeriodLabel.anchor(top: topAnchor, left: taskImage.rightAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 10, paddingBottom: 0, paddingRight: 0, width: frame.size.width / 2, height: 0, enableInsets: false)
+        taskPeriodLabel.centerYAnchor.constraint(equalTo:self.contentView.centerYAnchor).isActive = true
+        
+        
+        taskNextActionLabel.centerYAnchor.constraint(equalTo:self.contentView.centerYAnchor).isActive = true
+        taskNextActionLabel.anchor(top: topAnchor, left: taskImage.rightAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 150, paddingBottom: 0, paddingRight: 0, width: frame.size.width / 2, height: 0, enableInsets: false)
+        
+        
+        
+//         productDescriptionLabel.anchor(top: productNameLabel.bottomAnchor, left: productImage.rightAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 10, paddingBottom: 0, paddingRight: 0, width: frame.size.width / 2, height: 0, enableInsets: false)
+         
+         
+//         let stackView = UIStackView(arrangedSubviews: [decreaseButton,productQuantity,increaseButton])
+//         stackView.distribution = .equalSpacing
+//         stackView.axis = .horizontal
+//         stackView.spacing = 5
+//         addSubview(stackView)
+//         stackView.anchor(top: topAnchor, left: productNameLabel.rightAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 15, paddingLeft: 5, paddingBottom: 15, paddingRight: 10, width: 0, height: 70, enableInsets: false)
+//
+//         }
+//
+//         required init?(coder aDecoder: NSCoder) {
+//         fatalError(“init(coder:) has not been implemented”)
+//         }
+//
+//
+//        }
+
+     
+     
+     
+//     let stackView = UIStackView(arrangedSubviews: [decreaseButton,productQuantity,increaseButton])
+//     stackView.distribution = .equalSpacing
+//     stackView.axis = .horizontal
+//     stackView.spacing = 5
+//     addSubview(stackView)
+//     stackView.anchor(top: topAnchor, left: productNameLabel.rightAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 15, paddingLeft: 5, paddingBottom: 15, paddingRight: 10, width: 0, height: 70, enableInsets: false)
+//
+//     }
+     
+     
+    
+}
+    
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+    }
+}
